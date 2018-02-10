ea9a6ae41d62781abe6338a9bda2398d340ffedb
Viko1011 <vikotodorov666@gmail.com>
2018-02-10 19:19:06 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,56 @@
+function but0(){
+	document.getElementById('button1').style.backgroundColor = 'lightgray';
+	document.getElementById('button2').style.backgroundColor = 'lightgray';
+	document.getElementById('button3').style.backgroundColor = 'lightgray';
+	document.getElementById('button4').style.backgroundColor = 'lightgray';
+	document.getElementById('button5').style.backgroundColor = 'lightgray';
+	document.getElementById('button6').style.backgroundColor = 'lightgray';
+}
+function but1(){
+	document.getElementById('button0').style.backgroundColor = 'lightgray';
+	document.getElementById('button2').style.backgroundColor = 'lightgray';
+	document.getElementById('button3').style.backgroundColor = 'lightgray';
+	document.getElementById('button4').style.backgroundColor = 'lightgray';
+	document.getElementById('button5').style.backgroundColor = 'lightgray';
+	document.getElementById('button6').style.backgroundColor = 'lightgray';
+}
+function but2(){
+	document.getElementById('button0').style.backgroundColor = 'lightgray';
+	document.getElementById('button1').style.backgroundColor = 'lightgray';
+	document.getElementById('button3').style.backgroundColor = 'lightgray';
+	document.getElementById('button4').style.backgroundColor = 'lightgray';
+	document.getElementById('button5').style.backgroundColor = 'lightgray';
+	document.getElementById('button6').style.backgroundColor = 'lightgray';
+}
+function but3(){
+	document.getElementById('button0').style.backgroundColor = 'lightgray';
+	document.getElementById('button2').style.backgroundColor = 'lightgray';
+	document.getElementById('button1').style.backgroundColor = 'lightgray';
+	document.getElementById('button4').style.backgroundColor = 'lightgray';
+	document.getElementById('button5').style.backgroundColor = 'lightgray';
+	document.getElementById('button6').style.backgroundColor = 'lightgray';
+}
+function but4(){
+	document.getElementById('button1').style.backgroundColor = 'lightgray';
+	document.getElementById('button2').style.backgroundColor = 'lightgray';
+	document.getElementById('button3').style.backgroundColor = 'lightgray';
+	document.getElementById('button0').style.backgroundColor = 'lightgray';
+	document.getElementById('button5').style.backgroundColor = 'lightgray';
+	document.getElementById('button6').style.backgroundColor = 'lightgray';
+}
+function but5(){
+	document.getElementById('button1').style.backgroundColor = 'lightgray';
+	document.getElementById('button2').style.backgroundColor = 'lightgray';
+	document.getElementById('button3').style.backgroundColor = 'lightgray';
+	document.getElementById('button4').style.backgroundColor = 'lightgray';
+	document.getElementById('button0').style.backgroundColor = 'lightgray';
+	document.getElementById('button6').style.backgroundColor = 'lightgray';
+}
+function but6(){
+	document.getElementById('button1').style.backgroundColor = 'lightgray';
+	document.getElementById('button2').style.backgroundColor = 'lightgray';
+	document.getElementById('button3').style.backgroundColor = 'lightgray';
+	document.getElementById('button4').style.backgroundColor = 'lightgray';
+	document.getElementById('button5').style.backgroundColor = 'lightgray';
+	document.getElementById('button0').style.backgroundColor = 'lightgray';
+}
